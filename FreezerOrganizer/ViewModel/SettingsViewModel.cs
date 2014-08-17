f974a22e9b77f004e7e65de591ec5263003e64f9
Sabrina <sabrina_privat@hotmail.com>
2014-08-17 21:00:33 +0200

    Started implementing a settings view, which enables the user to choose a local XML file.

---
+++ FreezerOrganizer/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FreezerOrganizer.ViewModel
+{
+    class SettingsViewModel : ViewModelBase
+    {
+        private string _fileName;
+        private ICommand _openFileDialog;
+
+        public ICommand OpenFileDialog
+        {
+            get 
+            {
+                if (_openFileDialog == null)
+                {
+                    _openFileDialog = new RelayCommand(
+                        param => OpenFileDialog(),
+                        param => true // todo: necessary??
+                        );
+                }
+                return _openFileDialog;
+            }
+        }
+
+        private void OpenFileDialog()
+        {
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.DefaultExt = ".xml";
+            bool? isFileSelected = fileDialog.ShowDialog();
+
+            if (isFileSelected == true)
+            {
+                FileName = fileDialog.FileName;
+            }
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set 
+            {
+                if (_fileName != value)
+                {
+                    _fileName = value;
+                    OnPropertyChanged("FileName");
+                }
+            }
+        }
+    }
+}
